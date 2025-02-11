2f5d6c87adc09e599caf03843fd79d9a8466de9b
alwynmacabugwas <alwynmacabugwas@ICT-LPTP-22-02>
2025-02-11 16:16:01 +0800

    Add project files.

---
+++ Test2/Models/Test2DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test2.Models
+{
+    public class Test2DbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public Test2DbContext(DbContextOptions<Test2DbContext> options) : base(options)
+        {
+              
+        }
+    }
+}
