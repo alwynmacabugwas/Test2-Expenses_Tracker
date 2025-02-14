a9480c44eee27e37e1bf443cd75bb84db85f7875
Alwyn Macabugwas <alwyn.macabugwas@uap.asia>
2025-02-14 10:24:36 +0800

    Changed DB.

---
+++ Test2/Migrations/AppDb/20250214021312_4thmigratedAppDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test2.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class _4thmigratedAppDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
